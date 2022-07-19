848e904d330b753edb348b55c76e48cccc759468
Amy Ding <amyding04@gmail.com>
2022-07-19 15:59:07 -0500

    basic outline of homepage and added 2 segues from home page + new cocoa touch file for each view controller

---
+++ final project/informationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  informationViewController.swift
+//  final project
+//
+//  Created by Amy Ding on 2022-07-19.
+//
+
+import UIKit
+
+class informationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
